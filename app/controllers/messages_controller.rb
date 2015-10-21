e8d865b0fedfd3b1fb79e330ef719693ea7a7649
yukoyokouchi <yuko.yokouchi@kiramex.com>
2015-10-21 09:36:47 +0000

    hensyu

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,21 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
+  end
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
   end
   ## ここから追記
   def create
@@ -20,6 +33,9 @@ class MessagesController < ApplicationController
   private
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  def set_message
+    @message = Message.find(params[:id])
   end
   ## ここまで
 end
